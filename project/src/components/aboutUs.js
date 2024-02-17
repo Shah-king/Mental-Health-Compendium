e18896cb741ca588c046cdb44649814ae9b2b992
rafi-d-z <rafidizia@gmail.com>
2024-02-17 02:37:21 -0500

    added about us component with about us styling sheet

---
+++ project/src/components/aboutUs.js
@@ -0,0 +1,13 @@
+import './aboutUs.css'
+
+function aboutUs(){
+    return(
+        <section className='aboutUs'>
+
+            {/* You guys can code the html for the about us page here */}
+
+        </section>
+    );
+}
+
+export default aboutUs;
